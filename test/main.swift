710b2a58d100b2d377d0935ce161eb688ae147cf
EdoSarg <edsargsyan70@gmail.com>
2023-05-14 00:20:04 +0400

    test

---
+++ test/main.swift
@@ -0,0 +1,419 @@
+//
+//  main.swift
+//  test
+//
+//  Created by Edgar Sargsyan on 22.03.23.
+//
+
+//import Foundation
+/*
+print("Enter the name: ")
+
+if let anun = readLine() {
+
+    let contacts2 = [
+        ["name":"Karen", "phone":"112233", "mail":"aaa@aaaa.aa"],
+        ["name":"Ani", "phone":"556677", "mail":"bb@bb.aa"],
+        ["name":"Anna", "phone":"889900", "mail":"cc@cc.aa"],
+        ["name":"Ani", "phone":"446688", "mail":"aa@aa.aa"]
+    ]
+
+    
+    //if let an = updateValue"("556677", forKey: "phone1"){
+    //print("777777 \(an).")
+    
+    var b = false
+    
+    for kontact in contacts2{
+        if anun == kontact["name"]{
+            print(kontact["name"]!, kontact["phone"]!, kontact["mail"]!)
+            b = true
+            //break
+        }
+    }
+    
+    if b == false {
+        print("chka")
+    }
+}
+*/
+
+
+/*
+print("Enter the number:")
+
+if let input = readLine() {
+    
+    if let number = Int(input) {
+        
+        for _ in 0...number {
+            print("barew")
+        }
+        
+    }
+    
+}
+*/
+/*
+print("Enter the number:")
+if readLine != nil  {
+    if readLine != nil  {
+        
+       let x = 4
+     let y = 3
+        
+        for i in 0..<y {
+            for i2 in 0..<x {
+                print(0, terminator: "")
+            }
+            print("")
+        }
+        
+    }
+}
+
+*/
+
+/*
+
+
+print("Enter the number:")
+//var x = 0
+
+if let input = readLine() {
+    
+   // if Int(input) != nil {
+        // print(input)
+        
+        
+        
+        
+        print("Enter the number2:")
+        
+        if let input2 = readLine() {
+            
+         //   if Int(input2) != nil {
+                //     print(input2)
+              var  sum = 1
+                var t = 7
+                var y = 9
+                for i in 0..<sum {
+                    
+                    for i2 in 0..<input2 {
+                        print(0, terminator: "")
+                    }
+                    print("")
+                }
+                
+                
+            }
+        }
+    
+    
+*/
+
+/*
+import Foundation
+
+print("Enter your name : ", terminator:"")
+let input = readLine()
+print("Hello \(input!)")
+
+*/
+//                                 inputov * a anum /
+/*
+import Foundation
+//import Glibc
+print("Please enter the radius(r) of the circle:")
+if let readValue = readLine() {
+   if let radius = Int(readValue) {
+      let area = (22 * radius * radius)/7
+      print("Hence the area of the circle:", area)
+   }
+}
+
+*/
+/*
+
+
+import Foundation
+
+print("Enter the number 1:")
+
+if let typed = readLine() {
+    
+    let myInt1 = Int(typed) ?? 0
+    
+    print("Enter the number 2:")
+    
+    if let typed2 = readLine(){
+        
+        let myInt2 = Int(typed2) ?? 0
+        
+        for _ in 0..<myInt1{
+            for _ in 0..<myInt2{
+                print(0, terminator: "")
+            }
+            print("")
+        }
+
+    }
+}
+
+*/
+/*
+
+var i = 0
+var x = 6
+
+while i < x {
+    print("barev")
+    i += 3
+}
+*/
+/*
+
+
+
+import Foundation
+
+print("Enter the number 1:")
+
+if let typed = readLine() {
+    
+    let myInt1 = Int(typed) ?? 0
+    
+    print("Enter the number 2:")
+    
+    if let typed2 = readLine(){
+        
+        let myInt2 = Int(typed2) ?? 0
+        var x = 0
+        var y = 0
+        
+        while x < myInt1 {
+            
+            while y < myInt2 {
+                x += 1
+                y += 1
+                print(0, terminator: "")
+            }
+            
+            
+            
+            print("")
+        }
+    }
+}
+        */
+/*
+print("Enter the number 1:")
+
+if let typed = readLine() {
+    print("Enter the number 2:")
+    let myInt1 = Int(typed) ?? 0
+
+    if let typed2 = readLine(){
+        let myInt2 = Int(typed2) ?? 0
+        
+        var i = 0
+        while i < myInt1 {
+            
+            var y = 0
+            while y < myInt2 {
+                print(0,terminator: "")
+                y += 1
+            }
+            
+            i += 1
+            print("")
+        }
+    }
+}
+*/
+
+/*
+ //                CHI EXELLLL
+print("Enter the number 1:")
+
+if let typed = readLine() {
+    print("Enter the number 2:")
+    let myInt1 = Int(typed) ?? 0
+    
+    if let typed2 = readLine(){
+        let myInt2 = Int(typed2) ?? 0
+        
+        
+        var y = 0
+var nu = myInt1 * myInt2
+        while nu > y{
+                print(0,terminator: "")
+                 y += 1
+            }
+                //y += 1
+                print("")
+            
+        }
+}
+
+*/
+
+/*
+//termin aranc input
+var x = 6
+var y = 5
+
+var i = 1
+while i <= x * y {
+    print(0,terminator: "")
+    
+    if ( i % x == 0 ) {
+        print("")
+    }
+    
+    i += 1
+}
+
+
+*/
+
+
+
+/*
+var r = 6
+var d = 5
+var q = r * d
+var diapazon = 7...15
+var index = 0
+
+
+print("tiv")
+if let typed =  readLine () {
+    let myInt1 = Int(typed) ?? 0
+    while index < q {
+        print(0)
+        index += 1
+        //var index = range
+        
+        
+        
+        
+        while index < myInt1 {
+            print(q, 7...15)
+            /// while 7...myInt1 {
+            print("_",terminator: "")
+            index += 1
+        }
+    }
+}
+*/
+/*
+var x = 6
+var y = 5
+
+var i = 1
+while i <= x * y {
+    
+    if i <= x || i >= x * y - y || i % x == 0 || i % x == 1 {
+        print(0, terminator: "")
+    } else {
+        print(" ", terminator: "")
+    }
+        
+    if ( i % x == 0 ) {
+        print("")
+    }
+    
+    i += 1
+}
+*/
+/*
+var h = 6
+
+var i2 = 1
+
+while i2 <= 6 * 6{
+    if i2 <= h - 5 || i2 <= h - 4 {
+        print(0, terminator: "")
+   
+        if ( i2 % h == 0 ) {
+            print("")
+        }
+    }
+    i2 += 1
+}
+//0
+//00
+//000
+//0000
+//00000
+//000000
+*/
+
+
+//     input/ enum/
+/*
+print("Enter the name:")
+
+if readLine() != nil {
+    let anush = " (Naxkin hasce Norqi-2rd Zangvac)"
+    let arman = " (Naxkin hasce Norqi-2rd Zangvac)"
+    let edgar = " (Naxkin hasce Norqi-2rd Zangvac)"
+    let anushik = " (Naxkin hasce Norqi-2rd Zangvac)"
+    let artash = " (Naxkin hasce Norqi-2rd Zangvac)"
+    
+    
+    enum AndznakanTvyalner: String {
+        case Edgar = "Sargsyan Edgar Armani,Hasce_Dzoraxpyur"
+        case Arman = "Sargsyan Arman Ediki,Hasce_Dzoraxpyur"
+        case Artash = "Sargsyan Artash Armani,Hasce_Dzoraxpyur"
+        case Anushik = "Sargsyan Anushik Edgar,Hasce_Dzoraxpyur"
+        case Anush   =  "Smbatyan Anush Henzeli,Hasce_Dzoraxpyur"
+        
+    }
+    
+    let andznakantvyal = AndznakanTvyalner.Anush
+    print(andznakantvyal.rawValue + anush )
+    let andznakantvyal2 = AndznakanTvyalner.Arman
+    print(andznakantvyal2.rawValue + arman )
+    let andznakantvyal3 = AndznakanTvyalner.Anushik
+    print(andznakantvyal3.rawValue + anushik )
+    let andznakantvyal4 = AndznakanTvyalner.Artash
+    print(andznakantvyal4.rawValue + artash )
+    let andznakantvyal5 = AndznakanTvyalner.Edgar
+    print(andznakantvyal5.rawValue + edgar )
+    
+}
+*/
+/*
+print("Enter the name:")
+
+if readLine() != nil {
+    
+    
+    enum AndznakanTvyalner {
+        case Edgar
+        case Arman 
+        case Artash
+        
+    }
+    
+    let tvyalner = AndznakanTvyalner.Arman
+    switch tvyalner {
+        
+    case .Arman:
+        print( "Sargsyan Arman Ediki,Hasce_Dzoraxpyur")
+        //    switch tvyalner{
+        //    case .Edgar: print("Sargsyan Edgar Armani,Hasce_Dzoraxpyur")
+        //    case .Arman: print( "Sargsyan Arman Ediki,Hasce_Dzoraxpyur")
+        //    case .Artash: print( "Sargsyan Artash Armani,Hasce_Dzoraxpyur")
+        //
+        //    }
+    default:
+        print("Hakarak depqum tpi ***CHKA***")
+        
+    }
+}
+*/
+
+
